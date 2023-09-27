3545a2513b5c502c51f812a618df940572daeeda
Cleber Alberto <cleber.alberto@outlook.com>
2023-09-26 22:54:09 -0400

    Project setup - Clean Architecture

---
+++ src/WebApi/Program.cs
@@ -0,0 +1,33 @@
+﻿using Application;
+using Infrastructure;
+using Presentation;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+app.UseHttpsRedirection();
+
+app.Run();
+
